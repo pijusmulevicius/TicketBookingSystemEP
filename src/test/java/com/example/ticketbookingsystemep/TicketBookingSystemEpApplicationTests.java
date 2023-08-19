4930399eabb2926435e09904faeea3cf3784fd59
Pijus <muleviciuspijus40@gmail.com>
2023-08-19 21:51:23 +0300

    TBC-1 | initialize repository

---
+++ src/test/java/com/example/ticketbookingsystemep/TicketBookingSystemEpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketbookingsystemep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingSystemEpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
